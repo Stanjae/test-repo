fde2daa694784687c3f0f7bc853be6238b1f6316
stanjae <stanykhay29@gmail.com>
2025-10-27 00:43:37 +0100

    test03 rebase testing

--- src/pages/Tetris.tsx
+++ src/pages/Tetris.tsx
@@ -2,6 +2,9 @@ console.log("Tetris page loaded");
 
 export default function Tetris() {
     console.log("Rendering Tetris component");
+    
+    console.log("Rendering Tetris component2");
+    console.log("Rendering Tetris component3");
     return <div>
     <p>Sword -widows wail</p>
         <h2>Dire wolf</h2>
