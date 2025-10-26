2409791b35693904efdd24bc37ca9ac36a292c9c
stanley jaero <70924130+Stanjae@users.noreply.github.com>
2025-10-27 00:45:52 +0100

    Refactor Tetris component logging
    Removed unnecessary console logs and added new variables with logs.

--- src/pages/Tetris.tsx
+++ src/pages/Tetris.tsx
@@ -3,8 +3,11 @@ console.log("Tetris page loaded");
 export default function Tetris() {
     console.log("Rendering Tetris component");
     
-    console.log("Rendering Tetris component2");
-    console.log("Rendering Tetris component3");
+    const underdog = 'honorable vows';
+    console.log("Dunk and Egg", underdog);
+
+    const audible = 9;
+    console.log('lover of chicken', audible);
     return <div>
     <p>Sword -widows wail</p>
         <h2>Dire wolf</h2>
