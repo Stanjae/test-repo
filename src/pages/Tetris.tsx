b6670e02616fe72dda3cddab7089e58f005e4cda
stanjae <stanykhay29@gmail.com>
2025-10-27 00:35:22 +0100

    nights watch test-02

--- src/pages/Tetris.tsx
+++ src/pages/Tetris.tsx
@@ -1,6 +1,7 @@
 console.log("Tetris page loaded");
 
 export default function Tetris() {
+    console.log("Rendering Tetris component");
     return <div>
     <p>Sword -widows wail</p>
         <h2>Dire wolf</h2>
